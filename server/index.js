c221925cbda5a1fe3e18c9c82a9bc7dafa6bc602
Mitya Kim <mitya_kim@icloud.com>
2021-12-15 13:27:03 -0800

    css commit

--- server/index.js
+++ server/index.js
@@ -7,5 +7,11 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+})
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
 const port = process.env.PORT || 4005
 app.listen(port, () => {console.log(`port ${port}.`)})
